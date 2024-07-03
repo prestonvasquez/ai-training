8725937518fe585fa245833dda2ade8138cba106
William Kennedy <bill@ardanlabs.com>
2024-07-03 11:32:30 -0500

    code cleanup

--- examples/example4/main.go
+++ examples/example4/main.go
@@ -2,26 +2,12 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"github.com/ardanlabs/vector/foundation/mongodb"
 )
-
-type index struct {
-	ID   string `bson:"id"`
-	Type string `bson:"type"`
-}
-
-type document struct {
-	Text      string    `bson:"text"`
-	Embedding []float64 `bson:"embedding"`
-}
 
 func main() {
 	if err := run(); err != nil {
@@ -30,80 +16,55 @@ func main() {
 }
 
 func run() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
 
 	// -------------------------------------------------------------------------
 	// Connect to mongo
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-
-	auth := options.Client().SetAuth(options.Credential{
-		Username: "ardan",
-		Password: "ardan",
-	})
-
-	uri := options.Client().ApplyURI("mongodb://localhost:27017")
-
-	client, err := mongo.Connect(ctx, auth, uri)
+	client, err := mongodb.Connect(ctx, "mongodb://localhost:27017")
 	if err != nil {
-		return fmt.Errorf("connect: %w", err)
+		return fmt.Errorf("connectToMongo: %w", err)
 	}
 	defer client.Disconnect(ctx)
 
-	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-		return fmt.Errorf("ping: %w", err)
-	}
-
 	fmt.Println("Connected to MongoDB")
-
-	db := client.Database("example4")
 
 	// -------------------------------------------------------------------------
 	// Create collection
 
-	names, err := db.ListCollectionNames(ctx, bson.D{bson.E{Key: "name", Value: "book"}})
+	const dbName = "example4"
+	const collectionName = "book"
+
+	col, err := mongodb.CreateCollection(ctx, client, dbName, collectionName)
 	if err != nil {
-		return fmt.Errorf("list collections: %w", err)
+		return fmt.Errorf("createCollection: %w", err)
 	}
 
-	if len(names) == 0 {
-		fmt.Println("created book collection")
-
-		if err := client.Database("example4").CreateCollection(ctx, "book"); err != nil {
-			return fmt.Errorf("create collections: %w", err)
-		}
-	}
-
-	col := client.Database("example4").Collection("book")
+	fmt.Println("Created Collection")
 
 	// -------------------------------------------------------------------------
 	// Create vector index
 
-	indexes, err := lookupVectorIndex(ctx, col)
-	if err != nil {
-		return fmt.Errorf("lookupVectorIndex: %w", err)
+	settings := mongodb.VectorIndexSettings{
+		NumDimensions: 300,
+		Path:          "embedding",
+		Similarity:    "cosine",
 	}
 
-	if len(indexes) == 0 {
-		if err := createVectorIndex(ctx, col.Database()); err != nil {
-			return fmt.Errorf("createVectorIndex: %w", err)
-		}
-
-		fmt.Println("created vector index")
-
-		indexes, err = lookupVectorIndex(ctx, col)
-		if err != nil {
-			return fmt.Errorf("lookupVectorIndex: %w", err)
-		}
+	if err := mongodb.CreateVectorIndex(ctx, col, "vector_index", settings); err != nil {
+		return fmt.Errorf("createVectorIndex: %w", err)
 	}
 
-	if len(indexes) == 0 {
-		return errors.New("vector index does not exist")
-	}
-
-	fmt.Println(indexes)
+	fmt.Println("Created Vector Index")
 
 	// -------------------------------------------------------------------------
+	// Store some documents with their embeddings.
+
+	// type document struct {
+	// 	Text      string    `bson:"text"`
+	// 	Embedding []float64 `bson:"embedding"`
+	// }
 
 	// fmt.Println(res)
 
@@ -123,77 +84,4 @@ func run() error {
 	//col.FindOne(ctx, filter interface{}, opts ...*options.FindOneOptions)
 
 	return nil
-}
-
-func lookupVectorIndex(ctx context.Context, col *mongo.Collection) ([]index, error) {
-	indexName := "vector_index"
-	siv := col.SearchIndexes()
-	cur, err := siv.List(ctx, &options.SearchIndexesOptions{Name: &indexName})
-	if err != nil {
-		return nil, fmt.Errorf("index: %w", err)
-	}
-
-	var indexs []index
-
-	if err := cur.All(ctx, &indexs); err != nil {
-		return nil, fmt.Errorf("list: %w", err)
-	}
-
-	return indexs, nil
-}
-
-func createVectorIndex(ctx context.Context, db *mongo.Database) error {
-	f2 := bson.D{
-		{
-			Key:   "createSearchIndexes",
-			Value: "book",
-		},
-		{
-			Key: "indexes",
-			Value: []bson.D{
-				{
-					{
-						Key:   "name",
-						Value: "vector_index",
-					},
-					{
-						Key:   "type",
-						Value: "vectorSearch",
-					},
-					{
-						Key: "definition",
-						Value: bson.D{
-							{
-								Key: "fields",
-								Value: []bson.D{
-									{
-										{
-											Key:   "type",
-											Value: "vector",
-										},
-										{
-											Key:   "numDimensions",
-											Value: 300,
-										},
-										{
-											Key:   "path",
-											Value: "embedding",
-										},
-										{
-											Key:   "similarity",
-											Value: "cosine",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	res := db.RunCommand(ctx, f2)
-
-	return res.Err()
 }
---
+++ foundation/mongodb/mongodb.go
@@ -0,0 +1,156 @@
+package mongodb
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+// Connect attempts to connect to a mongo db instance.
+func Connect(ctx context.Context, host string) (*mongo.Client, error) {
+	auth := options.Client().SetAuth(options.Credential{
+		Username: "ardan",
+		Password: "ardan",
+	})
+
+	uri := options.Client().ApplyURI(host)
+
+	client, err := mongo.Connect(ctx, auth, uri)
+	if err != nil {
+		return nil, fmt.Errorf("connect: %w", err)
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, fmt.Errorf("ping: %w", err)
+	}
+
+	return client, nil
+}
+
+// CreateCollection will create the specified collection in the specified
+// database if it doesn't already exist.
+func CreateCollection(ctx context.Context, client *mongo.Client, dbName string, collectionName string) (*mongo.Collection, error) {
+	db := client.Database(dbName)
+
+	names, err := db.ListCollectionNames(ctx, bson.D{bson.E{Key: "name", Value: collectionName}})
+	if err != nil {
+		return nil, fmt.Errorf("list collections: %w", err)
+	}
+
+	if len(names) == 0 {
+		fmt.Println("created book collection")
+
+		if err := db.CreateCollection(ctx, collectionName); err != nil {
+			return nil, fmt.Errorf("create collections: %w", err)
+		}
+	}
+
+	return db.Collection(collectionName), nil
+}
+
+// CreateVectorIndex creates a very specific vector index for our example.
+func CreateVectorIndex(ctx context.Context, col *mongo.Collection, vectorIndexName string, settings VectorIndexSettings) error {
+	indexes, err := lookupVectorIndex(ctx, col)
+	if err != nil {
+		return fmt.Errorf("lookupVectorIndex: %w", err)
+	}
+
+	if len(indexes) == 0 {
+		if err := runCreateIndexCmd(ctx, col, vectorIndexName, settings); err != nil {
+			return fmt.Errorf("createVectorIndex: %w", err)
+		}
+
+		indexes, err = lookupVectorIndex(ctx, col)
+		if err != nil {
+			return fmt.Errorf("lookupVectorIndex: %w", err)
+		}
+	}
+
+	if len(indexes) == 0 {
+		return errors.New("vector index does not exist")
+	}
+
+	return nil
+}
+
+// =============================================================================
+
+func lookupVectorIndex(ctx context.Context, col *mongo.Collection) ([]Index, error) {
+	indexName := "vector_index"
+	siv := col.SearchIndexes()
+	cur, err := siv.List(ctx, &options.SearchIndexesOptions{Name: &indexName})
+	if err != nil {
+		return nil, fmt.Errorf("index: %w", err)
+	}
+
+	var indexs []Index
+
+	if err := cur.All(ctx, &indexs); err != nil {
+		return nil, fmt.Errorf("list: %w", err)
+	}
+
+	return indexs, nil
+}
+
+func runCreateIndexCmd(ctx context.Context, col *mongo.Collection, vectorIndexName string, settings VectorIndexSettings) error {
+	fields := bson.D{
+		{
+			Key:   "type",
+			Value: "vector",
+		},
+		{
+			Key:   "numDimensions",
+			Value: settings.NumDimensions,
+		},
+		{
+			Key:   "path",
+			Value: settings.Path,
+		},
+		{
+			Key:   "similarity",
+			Value: settings.Similarity,
+		},
+	}
+
+	idx := bson.D{
+		{
+			Key:   "createSearchIndexes",
+			Value: col.Name(),
+		},
+		{
+			Key: "indexes",
+			Value: []bson.D{
+				{
+					{
+						Key:   "name",
+						Value: vectorIndexName,
+					},
+					{
+						Key:   "type",
+						Value: "vectorSearch",
+					},
+					{
+						Key: "definition",
+						Value: bson.D{
+							{
+								Key: "fields",
+								Value: []bson.D{
+									fields,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	res := col.Database().RunCommand(ctx, idx)
+
+	return res.Err()
+}
